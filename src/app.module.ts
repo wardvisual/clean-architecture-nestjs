d50941c0ffbfc18b778b94f7c008d16e18978ab7
wardvisual <wardvisual@gmail.com>
2022-11-26 10:00:29 +0000

    :art: omit app service + controllers and format

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
+import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
+import { EnvironmentConfigModule } from './infrastructure/config/environment/environment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [EnvironmentConfigModule, TypeormModule, LoggerModule, ExceptionsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    EnvironmentConfigModule,
+    TypeOrmModule,
+    LoggerModule,
+    ExceptionsModule,
+    RepositoriesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
