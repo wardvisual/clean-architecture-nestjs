dd2bf00b699df9f61d85c1fe50905dff06b08820
wardvisual <wardvisual@gmail.com>
2022-11-27 07:37:46 +0000

    :necktie: add `execepionsfilter` to handle errors

---
+++ src/infrastructure/common/filter/exepcetion.filter.spec.ts
@@ -0,0 +1,7 @@
+import { AllExepcetionFilter } from './exepcetion.filter';
+
+describe('AllExepcetionFilter', () => {
+  it('should be defined', () => {
+    expect(new AllExepcetionFilter()).toBeDefined();
+  });
+});
