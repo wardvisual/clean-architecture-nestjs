3ef2feb27316e90dea97be6d9ba3a010db73e49c
wardvisual <wardvisual@gmail.com>
2022-11-27 08:01:48 +0000

    :necktie: add response and logging interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from '@/infrastructure/common/filter/exepcetion.filter';
-import { LoggerService } from './infrastructure/logger/logger.service';
+import { LoggerService } from '@/infrastructure/logger/logger.service';
+import { LoggingInterceptor } from '@/infrastructure/common/interceptors/logging/logging.interceptor';
+import { ResponseInterceptor } from '@/infrastructure/common/interceptors/response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(3000);
 }
